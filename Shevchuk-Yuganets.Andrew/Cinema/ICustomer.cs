846e036abdc1c2b4dae6ceef1503ccbb242c1a5d
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-06-23 23:15:00 +0300

    Added contracts

---
+++ Shevchuk-Yuganets.Andrew/Cinema/ICustomer.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+
+namespace *
+{
+	[ServiceContract(Namespace = "http://cinema.org/")]
+	public interface ICustomer
+	{
+		[OperationContract]
+		void ViewAvailableTickets();
+
+		[OperationContract]
+		void BookTicket();
+
+		[OperationContract]
+		void CancelBooking();
+
+		[OperationContract]
+		void AdjustBooking();
+
+		[OperationContract]
+		void LeaveFeedback();
+	}
+}
---
+++ Shevchuk-Yuganets.Andrew/Cinema/IModerator.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace *
+{
+	[ServiceContract(Namespace = "http://cinema.org/")]
+	public interface IModerator
+	{
+		[OperationContract]
+		void CreateShow();
+
+		[OperationContract]
+		void EditShow();
+
+		[OperationContract]
+		void ManageFeedback();
+	}
+}
