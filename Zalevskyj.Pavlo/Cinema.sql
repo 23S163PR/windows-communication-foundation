cbd22f66d3da070a4b88921c02eda1e0bbb8f92e
PavloZalevsky <pavlo.zalevsky@gmail.com>
2015-06-23 16:18:56 +0300

    update create database

--- Zalevskyj.Pavlo/Cinema.sql
+++ Zalevskyj.Pavlo/Cinema.sql
@@ -2,14 +2,12 @@ create database cinema
 use cinema
  
 create Table [Description](
-	[Id] int not null identity(1, 1),
+	[Id] int not null identity(1, 1) primary key,
 	[AboutShow] nvarchar(512) not null,
 	[Actors] nvarchar(512),
 	[Produsers] nvarchar(512),
 	[DurationInMinutes] int,
 	[Scenery] nvarchar(512)
-	
-	constraint pk_description_id primary key([Id]),
 )
 
 create Table [DescriptionGenres](
@@ -21,13 +19,10 @@ create Table [DescriptionGenres](
 )
 
 create Table [Shows](
-	[Id] int not null identity(1, 1),
+	[Id] int not null identity(1, 1) primary key,
 	[Title] nvarchar(512) not null,
 	[DescriptionId] int,
 	[CinemaHollHome] nvarchar(512),
-	
-	constraint pk_show_id primary key([Id]),
-	
 	
 	constraint fk_show_descriptionId_description_id
 	foreign key ([DescriptionId]) references [Description]([Id]),
@@ -35,11 +30,9 @@ create Table [Shows](
 
 
 create Table [PerformanceDays](
-	[Id] int not null identity(1, 1),
+	[Id] int not null identity(1, 1) primary key,
 	[DateTime] DateTime,
 	[ShowId] int,
-	
-	constraint pk_performanceDays_id primary key([Id]),
 
 	constraint fk_performanceDays_showId_show_id
 	foreign key ([ShowId]) references [Shows]([Id]),
