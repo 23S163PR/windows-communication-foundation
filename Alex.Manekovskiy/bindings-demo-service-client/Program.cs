2bb6bf6589e97d6a382205d5e1240127ceae61cf
Alex Manekovskiy <manekovskiy@gmail.com>
2015-06-23 17:54:52 +0300

    Added demo showing difference between basicHttpBinding and wsHttpBinding

---
+++ Alex.Manekovskiy/bindings-demo-service-client/Program.cs
@@ -0,0 +1,29 @@
+﻿using bindings_demo_service_client.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bindings_demo_service_client
+{
+
+    class Program
+    {
+        const string BasicHttpBindingEndpointName = "basicHttpBinding_CalculatorService";
+        const string WSHttpBindingEndpointName = "wsHttpBinding_CalculatorService";
+
+        static void Main(string[] args)
+        {
+            var basicHttpBindingClient = new CalculatorServiceClient(BasicHttpBindingEndpointName);
+            var result = basicHttpBindingClient.GetParrots(1);
+
+            Console.ReadLine();
+
+            var wsHttpBindingClient = new CalculatorServiceClient(WSHttpBindingEndpointName);
+            result = wsHttpBindingClient.GetParrots(1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Alex.Manekovskiy/bindings-demo-service-host/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bindings_demo_service_host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceHost = new ServiceHost(typeof(calculator_service.CalculatorService));
+            serviceHost.Open();
+
+            Console.WriteLine("Service started. Press Enter key to exit...");
+            Console.ReadLine();
+
+            serviceHost.Close();
+        }
+    }
+}
