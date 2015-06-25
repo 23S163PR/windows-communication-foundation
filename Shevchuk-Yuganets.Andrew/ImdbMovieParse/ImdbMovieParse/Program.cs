ab24f1c5c887b051e8e75be8c54d73050699bfbe
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-06-25 15:52:50 +0300

    Added xPath for movie posters

--- Shevchuk-Yuganets.Andrew/ImdbMovieParse/ImdbMovieParse/Program.cs
+++ Shevchuk-Yuganets.Andrew/ImdbMovieParse/ImdbMovieParse/Program.cs
@@ -6,14 +6,16 @@ namespace ImdbMovieParse
 {
 	class Program
 	{
-		private const string MovieListURL = "http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1";
+		private const string MovieListURL = "http://www.imdb.com/movies-in-theaters/";
 
 		private const string xPathForMovieName = "//*[@id='main']/div/div[2]/div/table/tbody/tr/td/h4/a";
 		private const string xPathForMovieDuration = "//*[@id='main']/div/div[2]/div/table/tbody/tr/td/p/time";
 		private const string xPathForMovieGenre = "//*[@id='main']/div/div[2]/div/table/tbody/tr/td/p/span";
 		private const string xPathForMovieDescription = "//*[@id='main']/div/div[2]/div/table/tbody/tr/td/div[@class='outline']";
 		private const string xPathForMovieDirector = "//*[@id='main']/div/div[2]/div/table/tbody/tr/td/div/span/span/a";
-		private const string xPathForMovieActors = "//*[@id='main']/div/div[2]/div/table/tbody/tr/td/div[@class='txt-block']/span/span/a"; 
+		private const string xPathForMovieActors = "//*[@id='main']/div/div[2]/div/table/tbody/tr/td/div[@class='txt-block']/span/span/a";
+		private const string xPathForMoviePoster = "//*[@id='img_primary']/div/a/div/img"; // .Attributes["src"].Value
+
 
 		static void Main(string[] args)
 		{
@@ -29,13 +31,12 @@ namespace ImdbMovieParse
 			var document = new HtmlDocument();
 			document.Load(responseStream);
 
-			var colection = document.DocumentNode.SelectNodes(xPathForMovieName);
+			var colection = document.DocumentNode.SelectNodes(xPathForMoviePoster);
 
 			foreach (var info in colection)
 			{
-				Console.WriteLine(info.InnerHtml);
+				Console.WriteLine(info.Attributes["src"].Value);
 			}
-
 		}
 	}
 }
